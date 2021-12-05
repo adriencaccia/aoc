adf77d0ad6e263cfa4d92129924a58094594804d
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2021-12-05 15:26:15 +0100

    feat: solve day 1 part 2

---
+++ days/1/part2/index.ts
@@ -0,0 +1,24 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+function main() {
+  const input = readFileSync(join(__dirname, "../input.txt"), "utf8");
+  const depths = input.split("\n").map(Number);
+  const slidingWindows: number[] = [];
+  for (let index = 0; index < depths.length - 2; index++) {
+    slidingWindows.push(depths[index] + depths[index + 1] + depths[index + 2]);
+  }
+
+  const answer = slidingWindows.reduce((acc, slidingWindow, index) => {
+    if (index === 0) {
+      return 0;
+    }
+
+    return slidingWindow > slidingWindows[index - 1] ? acc + 1 : acc;
+  }, 0);
+  console.log(answer);
+}
+
+main();
+
+// 1797
