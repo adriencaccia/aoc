0ef082fe316cb1c5cdfbfcf989146584fbc5a6aa
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2021-12-05 15:45:49 +0100

    feat: solve day 2 part 2

---
+++ days/2/part2/index.ts
@@ -0,0 +1,48 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+interface Instruction {
+  command: "forward" | "down" | "up";
+  value: number;
+}
+
+function main() {
+  const input = readFileSync(join(__dirname, "../input.txt"), "utf8");
+  const instructions = input.split("\n").map((instructionLine) => {
+    const [command, value] = instructionLine.split(" ");
+
+    const instruction: Instruction = {
+      command: command as Instruction["command"],
+      value: Number(value),
+    };
+
+    return instruction;
+  });
+
+  const { depth, horizontal } = instructions.reduce(
+    (acc, instruction) => {
+      switch (instruction.command) {
+        case "down":
+          acc.aim += instruction.value;
+          break;
+        case "up":
+          acc.aim -= instruction.value;
+          break;
+        case "forward":
+          acc.horizontal += instruction.value;
+          acc.depth += acc.aim * instruction.value;
+          break;
+      }
+
+      return acc;
+    },
+    { depth: 0, horizontal: 0, aim: 0 }
+  );
+  const answer = depth * horizontal;
+
+  console.log(answer);
+}
+
+main();
+
+// 1594785890
