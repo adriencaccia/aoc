ec6779f7667f5ed3060f4bdbdab7065c6ef35084
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2022-12-01 09:32:40 +0100

    feat(2022): solve day 1 part 2

---
+++ 2022/days/1/part2/index.ts
@@ -0,0 +1,30 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+function main() {
+  const input = readFileSync(join(__dirname, "../input.txt"), "utf8");
+  const caloriesList = input.split("\n").map(Number);
+  caloriesList.pop();
+
+  const elvesCalories: number[][] = [[]];
+  for (const calories of caloriesList) {
+    if (calories === 0) {
+      elvesCalories.push([]);
+      continue;
+    }
+
+    elvesCalories[elvesCalories.length - 1].push(calories);
+  }
+
+  const caloriesByElf = elvesCalories.map((elfCalories) =>
+    elfCalories.reduce((a, b) => a + b)
+  );
+
+  caloriesByElf.sort((a, b) => a - b).reverse();
+
+  console.log(caloriesByElf[0] + caloriesByElf[1] + caloriesByElf[2]);
+}
+
+main();
+
+// 197400
