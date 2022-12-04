fc452c8f0885ff1fdbac29659ea4f66f88426ec8
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2022-12-04 09:44:38 +0100

    feat(2022): solve day 4 part 2

---
+++ 2022/days/4/part2/index.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+function main() {
+  const input = readFileSync(join(__dirname, "../input.txt"), "utf8");
+
+  const elvesPairs = input
+    .split("\n")
+    .map((line) =>
+      line.split(",").map((cleaningLine) => cleaningLine.split("-").map(Number))
+    );
+  elvesPairs.pop();
+
+  const overlappingPairs = elvesPairs.filter(([firstElf, secondElf]) => {
+    const firstOverlapsWithSecond =
+      (secondElf[0] <= firstElf[0] && firstElf[0] <= secondElf[1]) ||
+      (secondElf[0] <= firstElf[1] && firstElf[1] <= secondElf[1]);
+
+    const secondOverlapsWithFirst =
+      (firstElf[0] <= secondElf[0] && secondElf[0] <= firstElf[1]) ||
+      (firstElf[0] <= secondElf[1] && secondElf[1] <= firstElf[1]);
+
+    return firstOverlapsWithSecond || secondOverlapsWithFirst;
+  });
+
+  console.log(overlappingPairs.length);
+}
+
+main();
+
+// 854
