a5909ae00661e25ef83e9617ae25db9a6124e19a
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2022-12-04 01:51:54 +0100

    feat(2022): solve day 3 part 2

---
+++ 2022/days/3/part2/index.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from "fs";
+import { chunk, intersection } from "lodash";
+import { join } from "path";
+
+function main() {
+  const input = readFileSync(join(__dirname, "../input.txt"), "utf8");
+  const rucksacks = input.split("\n").map((rucksack) => rucksack.split(""));
+  rucksacks.pop();
+
+  const threeRucksacksGroups = chunk(rucksacks, 3);
+
+  const answer = threeRucksacksGroups.reduce((sum, threeRucksacks) => {
+    const [itemPresentInAllRucksacks] = intersection(...threeRucksacks);
+
+    const itemCharCode = itemPresentInAllRucksacks.charCodeAt(0);
+
+    return sum + (itemCharCode >= 97 ? itemCharCode - 96 : itemCharCode - 38);
+  }, 0);
+
+  console.log(answer);
+}
+
+main();
+
+// 2425
