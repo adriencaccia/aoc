b8dda0691e87c89e9c6f4331728e9c45a52c3c12
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2022-12-25 14:59:16 +0100

    feat(2022): add day 21 part 1

--- 2022/days/21/part1/index.ts
+++ 2022/days/21/part1/index.ts
@@ -4,12 +4,62 @@ import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+interface Job {
+  operation: "+" | "-" | "*" | "/";
+  left: string;
+  right: string;
+}
+
+type Monkeys = Record<string, number | Job>;
+
+function prepareInput(input: string) {
+  const monkeys: Monkeys = {};
+  for (const line of input.split("\n")) {
+    const matches = line.match(/(\w+): (((\w+) (\+|-|\*|\/) (\w+))|(\d+))/);
+    if (matches === null) {
+      continue;
+    }
+    if (matches[7] === undefined) {
+      monkeys[matches[1]] = {
+        operation: matches[5] as Job["operation"],
+        left: matches[4],
+        right: matches[6],
+      };
+      continue;
+    }
+    monkeys[matches[1]] = Number(matches[7]);
+  }
+  return monkeys;
+}
+
+function calculate(monkeys: Monkeys, name = "root"): number {
+  const monkey = monkeys[name];
+  if (typeof monkey === "number") {
+    return monkey;
+  }
+  const { left, operation, right } = monkey;
+
+  switch (operation) {
+    case "*":
+      return calculate(monkeys, left) * calculate(monkeys, right);
+    case "+":
+      return calculate(monkeys, left) + calculate(monkeys, right);
+    case "-":
+      return calculate(monkeys, left) - calculate(monkeys, right);
+    case "/":
+      return calculate(monkeys, left) / calculate(monkeys, right);
+  }
+}
+
 function main() {
   const input = readFileSync(join(__dirname, "../input.txt"), "utf8");
+  const monkeys = prepareInput(input);
 
-  console.log();
+  const answer = calculate(monkeys);
+
+  console.log(answer);
 }
 
 main();
 
-// ???
+// 104272990112064
