7346f10615005836c3253cae8996aa4d4bdd7026
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-01 08:52:14 +0100

    feat(2023): day01 part 1

--- rust-2023/src/day01/mod.rs
+++ rust-2023/src/day01/mod.rs
@@ -1,16 +1,26 @@
 fn parse_input() -> Vec<u64> {
     include_str!("input.txt")
         .lines()
-        .filter_map(|line| line.parse().ok())
+        .map(|line| line.chars().filter(|char| char.is_ascii_digit()))
+        .map(|digits| {
+            let vec: Vec<char> = digits.collect();
+            let two_digits = format!(
+                "{:?}{:?}",
+                vec.first().unwrap().to_digit(10).unwrap(),
+                vec.last().unwrap().to_digit(10).unwrap()
+            );
+            two_digits.parse().unwrap()
+        })
         .collect()
 }
 
-pub fn main() {
+pub fn main() -> u64 {
     let input = parse_input();
 
-    let sum = input.iter().sum::<u64>();
+    let sum: u64 = input.iter().sum();
 
-    println!("sum {}", sum);
+    println!("part1 {}", sum);
+    sum
 }
 
 #[cfg(test)]
@@ -18,7 +28,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_main() {
-        main();
+    fn test_part_1() {
+        assert_eq!(main(), 54644);
     }
 }
