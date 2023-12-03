6c2b65b3d849e8913accc9077ee7f8b7a9a61c45
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-03 13:00:39 +0100

    feat(2023): add day03 part 2

--- rust-2023/src/day03/mod.rs
+++ rust-2023/src/day03/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use regex::Regex;
 
 fn get_char_at_index(input_string: &str, index: usize) -> Option<char> {
@@ -8,15 +10,16 @@ fn is_symbol(char: char) -> bool {
     !char.is_ascii_digit() && char != '.'
 }
 
-fn parse_input() -> u32 {
+fn parse_input() -> (u32, u32) {
     let matrix: Vec<&str> = include_str!("input.txt").lines().collect();
 
     let height = matrix.len();
     let width = matrix[0].len();
 
-    let mut sum = 0;
+    let mut part1 = 0;
 
     let numbers_regex = Regex::new(r"(\d+)").unwrap();
+    let mut gears: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
 
     for i in 0..height {
         let captures = numbers_regex.captures_iter(matrix[i]);
@@ -35,35 +38,84 @@ fn parse_input() -> u32 {
             let is_symbol_in_upper_string = i != 0
                 && matrix[i - 1]
                     .get(range_to_match.clone())
-                    .map_or(false, |sub| sub.chars().any(is_symbol));
+                    .map_or(false, |sub| {
+                        for (idx, char) in sub.char_indices() {
+                            if char == '*' {
+                                gears
+                                    .entry((i - 1, range_to_match.start + idx))
+                                    .or_default()
+                                    .push(number);
+                            }
+                        }
+
+                        sub.chars().any(is_symbol)
+                    });
             let is_symbol_in_lower_string = i + 1 < height
                 && matrix[i + 1]
                     .get(range_to_match.clone())
-                    .map_or(false, |sub| sub.chars().any(is_symbol));
-            let is_symbol_left = range.start != range_to_match.clone().start
-                && get_char_at_index(matrix[i], range_to_match.clone().start)
-                    .map_or(false, is_symbol);
-            let is_symbol_right = range.end != range_to_match.clone().end
-                && get_char_at_index(matrix[i], range_to_match.end - 1).map_or(false, is_symbol);
+                    .map_or(false, |sub| {
+                        for (idx, char) in sub.char_indices() {
+                            if char == '*' {
+                                gears
+                                    .entry((i + 1, range_to_match.start + idx))
+                                    .or_default()
+                                    .push(number);
+                            }
+                        }
+
+                        sub.chars().any(is_symbol)
+                    });
+            let is_symbol_left = range.start != range_to_match.start
+                && get_char_at_index(matrix[i], range_to_match.start).map_or(false, |char| {
+                    if char == '*' {
+                        gears
+                            .entry((i, range_to_match.start))
+                            .or_default()
+                            .push(number);
+                    }
+                    is_symbol(char)
+                });
+            let is_symbol_right = range.end != range_to_match.end
+                && get_char_at_index(matrix[i], range_to_match.end - 1).map_or(false, |char| {
+                    if char == '*' {
+                        gears
+                            .entry((i, range_to_match.end - 1))
+                            .or_default()
+                            .push(number);
+                    }
+                    is_symbol(char)
+                });
 
             if is_symbol_in_lower_string
                 || is_symbol_in_upper_string
                 || is_symbol_left
                 || is_symbol_right
             {
-                sum += number;
+                part1 += number;
             }
         }
     }
 
-    sum
+    let part2 = gears
+        .values()
+        .map(|parts| {
+            if parts.len() != 2 {
+                return 0;
+            }
+
+            parts[0] * parts[1]
+        })
+        .sum();
+
+    (part1, part2)
 }
 
 pub fn main() -> (u32, u32) {
-    let part1 = parse_input();
+    let (part1, part2) = parse_input();
     println!("part1 {}", part1);
+    println!("part2 {}", part2);
 
-    (part1, 0)
+    (part1, part2)
 }
 
 #[cfg(test)]
@@ -75,6 +127,6 @@ mod tests {
         let (part1, part2) = main();
 
         assert_eq!(part1, 527364);
-        assert_eq!(part2, 0);
+        assert_eq!(part2, 79026871);
     }
 }
