f90e6b644ba6d5f8cc24c05820eb507efa415158
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-03 20:54:27 +0100

    feat: update init script with rust and current day

--- scripts/init.js
+++ scripts/init.js
@@ -2,15 +2,28 @@ import { execSync } from "child_process";
 import shelljs from "shelljs";
 
 // usage:
-// node scripts/init.js 2022 19
+// init the current day
+// node scripts/init.js rust|ts
 
-const [, , year, day] = process.argv;
+// init any day
+// node scripts/init.js 2022 19 rust|ts
+
+let [, , year, day, lang] = process.argv;
+
+if (["rust", "ts"].includes(year)) {
+  lang = year;
+  const now = new Date();
+  year = now.getFullYear();
+  day = now.getDate().toString();
+}
 
 const dayPath = `${year}/src/day${day.padStart(2, "0")}`;
 
-console.log(`Creating file structure for year ${year} day ${day}...`);
+console.log(
+  `Creating file structure for year ${year} day ${day} in ${lang}...`
+);
 shelljs.mkdir("-p", dayPath);
-shelljs.cp("-r", "template/*", dayPath);
+shelljs.cp("-r", `template/${lang}/*`, dayPath);
 
 execSync(
   `aoc download --year ${year} --day ${day} --overwrite \
---
+++ template/rust/mod.rs
@@ -0,0 +1,40 @@
+fn parse_input(input: &str) -> (u32, u32) {
+    let lines = input.trim().lines();
+
+    let part1 = 0;
+    let part2 = 0;
+    (part1, part2)
+}
+
+pub fn main() -> (u32, u32) {
+    let (part1, part2) = parse_input(include_str!("input.txt"));
+    println!("part1 {}", part1);
+    println!("part2 {}", part2);
+
+    (part1, part2)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = r#"
+<REPLACE ME>
+"#;
+
+    #[test]
+    fn test_example() {
+        let (part1, part2) = parse_input(EXAMPLE_INPUT.trim());
+
+        assert_eq!(part1, 0);
+        assert_eq!(part2, 0);
+    }
+
+    #[test]
+    fn test_main() {
+        let (part1, part2) = main();
+
+        assert_eq!(part1, 0);
+        assert_eq!(part2, 0);
+    }
+}
