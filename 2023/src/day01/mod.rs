8c01ff7829d64c111b932bfffac5d49bcff9ad23
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-03 19:21:24 +0100

    refactor(2023): day01

--- 2023/src/day01/mod.rs
+++ 2023/src/day01/mod.rs
@@ -1,101 +1,110 @@
-fn parse_input() -> impl Iterator<Item = u32> {
-    include_str!("input.txt").lines().map(|line| {
-        let digits = line.chars().filter_map(|char| char.to_digit(10));
-        digits.clone().next().unwrap() * 10 + digits.last().unwrap()
-    })
-}
-
-fn parse_input_part_2() -> Vec<u64> {
-    include_str!("input.txt")
+fn parse_input(input: &str) -> (u32, u32) {
+    let (part1, part2): (Vec<u32>, Vec<u32>) = input
         .lines()
         .map(|line| {
-            let chars: Vec<char> = line.chars().collect();
+            let mut digits = line.chars().filter_map(|char| char.to_digit(10));
+            let first = digits.next().unwrap();
 
-            let mut digits: Vec<u64> = vec![];
-            let mut i = 0;
-            while i < chars.len() {
-                if let Some(digit) = chars[i].to_digit(10) {
-                    digits.push(digit.into());
-                    i += 1;
-                    continue;
-                }
-                if (i + 3) <= chars.len() && chars[i..i + 3].iter().collect::<String>() == "one" {
-                    digits.push(1);
-                    i += 1;
-                    continue;
-                }
-                if (i + 3) <= chars.len() && chars[i..i + 3].iter().collect::<String>() == "two" {
-                    digits.push(2);
-                    i += 1;
-                    continue;
-                }
-                if (i + 5) <= chars.len() && chars[i..i + 5].iter().collect::<String>() == "three" {
-                    digits.push(3);
-                    i += 1;
-                    continue;
-                }
-                if (i + 4) <= chars.len() && chars[i..i + 4].iter().collect::<String>() == "four" {
-                    digits.push(4);
-                    i += 1;
-                    continue;
-                }
-                if (i + 4) <= chars.len() && chars[i..i + 4].iter().collect::<String>() == "five" {
-                    digits.push(5);
-                    i += 1;
-                    continue;
-                }
-                if (i + 3) <= chars.len() && chars[i..i + 3].iter().collect::<String>() == "six" {
-                    digits.push(6);
-                    i += 1;
-                    continue;
-                }
-                if (i + 5) <= chars.len() && chars[i..i + 5].iter().collect::<String>() == "seven" {
-                    digits.push(7);
-                    i += 1;
-                    continue;
-                }
-                if (i + 5) <= chars.len() && chars[i..i + 5].iter().collect::<String>() == "eight" {
-                    digits.push(8);
-                    i += 1;
-                    continue;
-                }
-                if (i + 4) <= chars.len() && chars[i..i + 4].iter().collect::<String>() == "nine" {
-                    digits.push(9);
-                    i += 1;
-                    continue;
-                }
-                i += 1;
-            }
+            let part1 = match digits.last() {
+                Some(last) => first * 10 + last,
+                None => first * 10 + first,
+            };
 
-            let two_digits = format!("{:?}{:?}", digits.first().unwrap(), digits.last().unwrap());
-            two_digits.parse().unwrap()
+            let numbers: Vec<String> = vec![
+                "one".into(),
+                "two".into(),
+                "three".into(),
+                "four".into(),
+                "five".into(),
+                "six".into(),
+                "seven".into(),
+                "eight".into(),
+                "nine".into(),
+            ];
+            let first = find_in_string(line, &numbers);
+
+            let reversed_line = line.chars().rev().collect::<String>();
+            let reversed_numbers: Vec<String> = numbers
+                .into_iter()
+                .map(|number| number.chars().rev().collect::<String>())
+                .collect();
+            let last = find_in_string(&reversed_line, &reversed_numbers);
+
+            let part2 = first * 10 + last;
+
+            (part1, part2)
         })
-        .collect()
+        .unzip();
+
+    (part1.iter().sum(), part2.iter().sum())
 }
 
-pub fn main() -> (u32, u64) {
-    let input = parse_input();
-    let part1 = input.sum();
-    println!("part1 {}", part1);
+fn find_in_string(line: &str, numbers: &[String]) -> u32 {
+    for (idx, char) in line.chars().enumerate() {
+        if let Some(digit) = char.to_digit(10) {
+            return digit;
+        }
 
-    let input2 = parse_input_part_2();
-    let part2: u64 = input2.iter().sum();
+        for (value, number) in numbers.iter().enumerate() {
+            if line.get(0..idx + 1).unwrap().contains(number) {
+                return value as u32 + 1;
+            }
+        }
+    }
+
+    panic!("No digit or number found in {}", line)
+}
+
+pub fn main() -> (u32, u32) {
+    let (part1, part2) = parse_input(include_str!("input.txt"));
+    println!("part1 {}", part1);
     println!("part2 {}", part2);
 
     (part1, part2)
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_part_1() {
-        assert_eq!(main().0, 54644);
+    fn test_example_part1() {
+        let (part1, _part2) = parse_input(
+            r#"
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+        "#
+            .trim(),
+        );
+
+        assert_eq!(part1, 142);
     }
 
     #[test]
-    fn test_part_2() {
-        assert_eq!(main().1, 53348);
+    fn test_example_part2() {
+        let (_part1, part2) = parse_input(
+            // ! Added a 1 to the second line to make the part1 algo work
+            r#"
+two1nine
+eightwo1three
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+        "#
+            .trim(),
+        );
+
+        assert_eq!(part2, 281);
+    }
+
+    #[test]
+    fn test_main() {
+        let (part1, part2) = main();
+
+        assert_eq!(part1, 54644);
+        assert_eq!(part2, 53348);
     }
 }
