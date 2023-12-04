0ff8bd42377c255bb21adbad55b435a03825ab15
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-04 09:21:35 +0100

    feat(2023): add day04 part 2

--- 2023/src/day04/mod.rs
+++ 2023/src/day04/mod.rs
@@ -1,29 +1,28 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 fn parse_input(input: &str) -> (u32, u32) {
     let lines = input.trim().lines();
 
+    let mut scratch_copies: HashMap<usize, u32> = HashMap::new();
+
     let part1 = lines
-        .map(|line| {
+        .enumerate()
+        .map(|(idx, line)| {
             let (winning, mine) = line
                 .split(':')
                 .last()
                 .unwrap()
                 .split('|')
                 .map(|numbers| {
-                    let numbers = numbers.split(' ');
-                    let trimmed = numbers
+                    numbers
+                        .split(' ')
                         .map(|n| n.trim())
                         .filter(|n| !n.is_empty())
-                        .collect_vec();
-
-                    trimmed.into_iter()
                 })
                 .collect_tuple()
                 .unwrap();
-
-            // println!("winning {:?}", winning.clone().join(" "));
-            // println!("mine    {:?}", mine.clone().join(" "));
 
             let number_of_wins: u32 = mine
                 .filter(|number| winning.clone().any(|win| &win == number))
@@ -32,8 +31,10 @@ fn parse_input(input: &str) -> (u32, u32) {
                 .try_into()
                 .unwrap();
 
-            // println!("{} matches", number_of_wins);
-            // println!("=============================");
+            let number_of_copies = scratch_copies.entry(idx).or_insert(1).to_owned();
+            for i in 0..number_of_wins {
+                *scratch_copies.entry(idx + i as usize + 1).or_insert(1) += number_of_copies;
+            }
 
             match number_of_wins {
                 0 => 0,
@@ -42,7 +43,7 @@ fn parse_input(input: &str) -> (u32, u32) {
         })
         .sum();
 
-    let part2 = 0;
+    let part2 = scratch_copies.values().sum();
     (part1, part2)
 }
 
@@ -72,14 +73,14 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
         let (part1, part2) = parse_input(EXAMPLE_INPUT.trim());
 
         assert_eq!(part1, 13);
-        assert_eq!(part2, 0);
+        assert_eq!(part2, 30);
     }
 
     #[test]
     fn test_main() {
         let (part1, part2) = main();
 
-        assert_eq!(part1, 0);
-        assert_eq!(part2, 0);
+        assert_eq!(part1, 24706);
+        assert_eq!(part2, 13114317);
     }
 }
