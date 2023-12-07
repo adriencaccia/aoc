39a09099f42444f16f75f2e52afd36f79d97163d
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-07 14:09:34 +0100

    refactor(2023): refactor and simplify day07

--- 2023/src/day07/mod.rs
+++ 2023/src/day07/mod.rs
@@ -45,7 +45,7 @@ struct Hand {
 }
 
 impl Hand {
-    pub fn from_line(line: &str) -> Self {
+    pub fn from_line(line: &str, joker: bool) -> Self {
         let (cards_str, bid_str) = line.split_whitespace().collect_tuple().unwrap();
         let mut card_counter: HashMap<char, u8> = HashMap::new();
         for char in cards_str.chars() {
@@ -54,54 +54,26 @@ impl Hand {
                 .and_modify(|n| *n += 1)
                 .or_insert(1);
         }
-        let hand_type = match (
-            card_counter.len(),
-            card_counter.values().any(|&n| n == 3),
-            card_counter.values().any(|&n| n == 4),
+        if let (true, false, Some(j_count)) = (
+            joker,
+            card_counter.len() == 1,
+            card_counter.get(&'J').cloned(),
         ) {
-            (1, _, _) => HandType::FiveKind,
-            (2, _, true) => HandType::FourKind,
-            (2, _, false) => HandType::FullHouse,
-            (3, true, _) => HandType::ThreeKind,
-            (3, false, _) => HandType::TwoPair,
-            (4, _, _) => HandType::OnePair,
-            _ => HandType::HighCard,
-        };
-
-        Self {
-            cards: cards_str.into(),
-            hand_type,
-            bid: bid_str.parse().unwrap(),
+            let (max_key, _) = card_counter
+                .iter()
+                .filter(|(&k, _)| k != 'J')
+                .max_by_key(|(_, &v)| v)
+                .unwrap();
+            card_counter.entry(*max_key).and_modify(|v| *v += j_count);
+            card_counter.remove_entry(&'J');
         }
-    }
-    pub fn from_line_joker(line: &str) -> Self {
-        let (cards_str, bid_str) = line.split_whitespace().collect_tuple().unwrap();
-        let mut card_counter: HashMap<char, u8> = HashMap::new();
-        for char in cards_str.chars() {
-            card_counter
-                .entry(char)
-                .and_modify(|n| *n += 1)
-                .or_insert(1);
-        }
-
-        let hand_type = match (
-            card_counter.len(),
-            card_counter.values().any(|&n| n == 3),
-            card_counter.values().any(|&n| n == 4),
-            card_counter.get(&'J'),
-        ) {
-            (1, _, _, _) => HandType::FiveKind,
-            (2, _, _, Some(_)) => HandType::FiveKind,
-            (2, _, true, _) => HandType::FourKind,
-            (2, _, _, _) => HandType::FullHouse,
-            (3, true, _, Some(_)) => HandType::FourKind,
-            (3, true, _, _) => HandType::ThreeKind,
-            (3, _, _, Some(2)) => HandType::FourKind,
-            (3, _, _, Some(1)) => HandType::FullHouse,
-            (3, _, _, _) => HandType::TwoPair,
-            (4, _, _, Some(_)) => HandType::ThreeKind,
-            (4, _, _, _) => HandType::OnePair,
-            (_, _, _, Some(_)) => HandType::OnePair,
+        let hand_type = match card_counter.len() {
+            1 => HandType::FiveKind,
+            2 if card_counter.values().any(|&n| n == 4) => HandType::FourKind,
+            2 => HandType::FullHouse,
+            3 if card_counter.values().any(|&n| n == 3) => HandType::ThreeKind,
+            3 => HandType::TwoPair,
+            4 => HandType::OnePair,
             _ => HandType::HighCard,
         };
 
@@ -112,48 +84,19 @@ impl Hand {
         }
     }
 
-    fn cmp_joker(&self, other: &Self) -> Ordering {
+    fn cmp_with_order(&self, other: &Self, card_order: &HashMap<char, u8>) -> Ordering {
         let self_type_rank = HAND_TYPE_ORDER.get(&self.hand_type).unwrap();
         let other_type_rank = HAND_TYPE_ORDER.get(&other.hand_type).unwrap();
 
         let order_rank = self_type_rank.cmp(other_type_rank);
         if order_rank == Ordering::Equal {
-            let self_cards_ranks = self
-                .cards
-                .chars()
-                .map(|c| CARD_ORDER_JOKER.get(&c).unwrap());
-            let other_cards_ranks = other
-                .cards
-                .chars()
-                .map(|c| CARD_ORDER_JOKER.get(&c).unwrap());
+            let self_cards_ranks = self.cards.chars().map(|c| card_order.get(&c).unwrap());
+            let other_cards_ranks = other.cards.chars().map(|c| card_order.get(&c).unwrap());
 
             return self_cards_ranks.cmp(other_cards_ranks);
         }
 
         order_rank
-    }
-}
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let self_type_rank = HAND_TYPE_ORDER.get(&self.hand_type).unwrap();
-        let other_type_rank = HAND_TYPE_ORDER.get(&other.hand_type).unwrap();
-
-        let order_rank = self_type_rank.cmp(other_type_rank);
-        if order_rank == Ordering::Equal {
-            let self_cards_ranks = self.cards.chars().map(|c| CARD_ORDER.get(&c).unwrap());
-            let other_cards_ranks = other.cards.chars().map(|c| CARD_ORDER.get(&c).unwrap());
-
-            return self_cards_ranks.cmp(other_cards_ranks);
-        }
-
-        order_rank
-    }
-}
-
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
     }
 }
 
@@ -162,15 +105,15 @@ fn parse_input(input: &str) -> (u32, u32) {
 
     let part1 = lines
         .clone()
-        .map(Hand::from_line)
-        .sorted()
+        .map(|line| Hand::from_line(line, false))
+        .sorted_by(|a, b| Hand::cmp_with_order(a, b, &CARD_ORDER))
         .enumerate()
         .fold(0, |acc, (rank, hand)| acc + hand.bid * (rank as u32 + 1));
 
     let part2 = lines
         .clone()
-        .map(Hand::from_line_joker)
-        .sorted_by(Hand::cmp_joker)
+        .map(|line| Hand::from_line(line, true))
+        .sorted_by(|a, b| Hand::cmp_with_order(a, b, &CARD_ORDER_JOKER))
         .enumerate()
         .fold(0, |acc, (rank, hand)| acc + hand.bid * (rank as u32 + 1));
 
@@ -213,36 +156,5 @@ mod tests {
 
         assert_eq!(part1, 256448566);
         assert_eq!(part2, 254412181);
-    }
-
-    #[test]
-    fn test_order_joker() {
-        let hand1 = Hand {
-            bid: 1,
-            cards: "JKKK2".into(),
-            hand_type: HandType::FourKind,
-        };
-        let hand2 = Hand {
-            bid: 1,
-            cards: "QQQQ2".into(),
-            hand_type: HandType::FourKind,
-        };
-
-        assert_eq!(hand1.cmp(&hand2), Ordering::Less);
-        assert_eq!(hand1.cmp_joker(&hand2), Ordering::Less);
-
-        let hand1 = Hand {
-            bid: 1,
-            cards: "JKKK2".into(),
-            hand_type: HandType::FourKind,
-        };
-        let hand2 = Hand {
-            bid: 1,
-            cards: "99992".into(),
-            hand_type: HandType::FourKind,
-        };
-
-        assert_eq!(hand1.cmp(&hand2), Ordering::Greater);
-        assert_eq!(hand1.cmp_joker(&hand2), Ordering::Less);
     }
 }
