f9de05e514b66504dba4644582b4f6f3e019267a
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-09 13:24:43 +0100

    feat(2023): add day09 part 2

--- 2023/src/day09/mod.rs
+++ 2023/src/day09/mod.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 
-fn parse_input(input: &str) -> (i32, u32) {
+fn parse_input(input: &str) -> (i32, i32) {
     let lines: Vec<Vec<i32>> = input
         .trim()
         .lines()
@@ -12,7 +12,7 @@ fn parse_input(input: &str) -> (i32, u32) {
         .collect_vec();
 
     let mut part1 = 0;
-    for mut line in lines {
+    for mut line in lines.clone() {
         let mut acc = 0;
         let mut start_idx = 1;
         while *line.last().unwrap() != 0 {
@@ -26,14 +26,29 @@ fn parse_input(input: &str) -> (i32, u32) {
             start_idx += 1;
             acc += value_to_add_end;
         }
-        part1 += acc
+        part1 += acc;
     }
 
-    let part2 = 0;
+    let mut part2 = 0;
+    for mut line in lines {
+        let mut start_idx = 1;
+        let mut start_values: Vec<i32> = Vec::new();
+        while *line.last().unwrap() != 0 {
+            start_values.push(line[start_idx - 1]);
+            let mut value_to_subtract = line[start_idx - 1];
+            (start_idx..line.len()).for_each(|idx| {
+                let new_value = line[idx] - value_to_subtract;
+                value_to_subtract = line[idx];
+                line[idx] = new_value;
+            });
+            start_idx += 1;
+        }
+        part2 += start_values.iter().rev().fold(0, |acc, n| n - acc);
+    }
     (part1, part2)
 }
 
-pub fn main() -> (i32, u32) {
+pub fn main() -> (i32, i32) {
     let (part1, part2) = parse_input(include_str!("input.txt"));
     println!("part1 {}", part1);
     println!("part2 {}", part2);
@@ -58,7 +73,7 @@ mod tests {
         let (part1, part2) = parse_input(EXAMPLE_INPUT);
 
         assert_eq!(part1, 114);
-        assert_eq!(part2, 0);
+        assert_eq!(part2, 2);
     }
 
     #[test]
@@ -66,6 +81,6 @@ mod tests {
         let (part1, part2) = main();
 
         assert_eq!(part1, 1868368343);
-        assert_eq!(part2, 0);
+        assert_eq!(part2, 1022);
     }
 }
