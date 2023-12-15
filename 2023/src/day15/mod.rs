e94b5d61c1512d6ea0c1a0ccdb7e8e2989e39cb5
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-15 09:37:33 +0100

    feat(2023): add day15 part 2

--- 2023/src/day15/mod.rs
+++ 2023/src/day15/mod.rs
@@ -1,11 +1,67 @@
+use itertools::Itertools;
+use parse_display::{Display, FromStr};
+
+#[derive(Display, FromStr, PartialEq, Eq, Hash, Debug, Clone)]
+enum Operation {
+    #[display("{label}-")]
+    Remove { label: String },
+    #[display("{label}={focal_length}")]
+    Set { label: String, focal_length: u32 },
+}
+
+fn hash(s: &str) -> u32 {
+    s.chars().fold(0, |acc, c| ((acc + c as u32) * 17) % 256)
+}
+
 fn parse_input(input: &str) -> (u32, u32) {
-    let part1 = input
+    let part1 = input.trim().split(',').map(hash).sum();
+
+    let mut boxes: Vec<Vec<(String, u32)>> = vec![vec![]; 256];
+
+    for operation in input
         .trim()
         .split(',')
-        .map(|s| s.chars().fold(0, |acc, c| ((acc + c as u32) * 17) % 256))
-        .sum();
+        .map(|s| s.parse::<Operation>().unwrap())
+    {
+        match operation {
+            Operation::Remove { label } => {
+                let box_idx = hash(label.as_str()) as usize;
+                if let Some((label_position, _)) = boxes[box_idx]
+                    .iter()
+                    .find_position(|(l, _)| l == label.as_str())
+                {
+                    boxes[box_idx].remove(label_position);
+                }
+            }
+            Operation::Set {
+                label,
+                focal_length,
+            } => {
+                let box_idx = hash(label.as_str()) as usize;
+                if let Some((label_position, _)) = boxes[box_idx]
+                    .iter()
+                    .find_position(|(l, _)| l == label.as_str())
+                {
+                    boxes[box_idx][label_position].1 = focal_length;
+                } else {
+                    boxes[box_idx].push((label, focal_length));
+                }
+            }
+        }
+    }
 
-    let part2 = 0;
+    let part2 = boxes
+        .into_iter()
+        .enumerate()
+        .fold(0, |acc, (box_idx, operations)| {
+            acc + operations
+                .into_iter()
+                .enumerate()
+                .map(|(slot_idx, (_, focal_length))| {
+                    (box_idx as u32 + 1) * (slot_idx as u32 + 1) * focal_length
+                })
+                .sum::<u32>()
+        });
     (part1, part2)
 }
 
@@ -32,7 +88,7 @@ mod tests {
         let (part1, part2) = parse_input(EXAMPLE_INPUT);
 
         assert_eq!(part1, 1320);
-        assert_eq!(part2, 0);
+        assert_eq!(part2, 145);
     }
 
     #[test]
@@ -40,6 +96,6 @@ mod tests {
         let (part1, part2) = main();
 
         assert_eq!(part1, 512283);
-        assert_eq!(part2, 0);
+        assert_eq!(part2, 215827);
     }
 }
