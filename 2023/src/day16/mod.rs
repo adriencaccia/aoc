a5530b2a826eabd87e3e3260511c64acf5c56e50
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-16 23:16:23 +0100

    refactor(2023): simplify code and parallelize with rayon

--- 2023/src/day16/mod.rs
+++ 2023/src/day16/mod.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 use parse_display::{Display, FromStr};
-use strum::{EnumIter, IntoEnumIterator};
+use rayon::prelude::*;
 
 #[derive(Display, FromStr, PartialEq, Eq, Hash, Debug, Clone, Copy)]
 enum Tile {
@@ -18,7 +18,7 @@ enum Tile {
     SplitV,
 }
 
-#[derive(EnumIter, Eq, PartialEq, Hash, Debug, Copy, Clone)]
+#[derive(Eq, PartialEq, Hash, Debug, Copy, Clone)]
 enum Direction {
     North,
     West,
@@ -26,95 +26,63 @@ enum Direction {
     East,
 }
 
-fn get_energized_tiles(grid: &Vec<Vec<Tile>>, beam: ((isize, isize), Direction)) -> u32 {
+fn get_energized_tiles(grid: &Vec<Vec<Tile>>, beam: (isize, isize, Direction)) -> u32 {
     let mut energized_grid: Vec<Vec<bool>> = vec![vec![false; grid[0].len()]; grid.len()];
-    let mut visited: HashSet<((isize, isize), Direction)> = HashSet::new();
-    let mut beams: Vec<((isize, isize), Direction)> = vec![beam];
+    let mut visited: HashSet<(isize, isize, Direction)> = HashSet::new();
+    let mut beams: Vec<(isize, isize, Direction)> = vec![beam];
 
     loop {
-        if beams.iter().all(|&((x, y), direction)| {
-            x < 0
-                || grid.len() <= (x as usize)
-                || y < 0
-                || grid[0].len() <= (y as usize)
-                || visited.contains(&((x, y), direction))
-        }) {
+        if beams.is_empty() {
             return energized_grid
                 .into_iter()
                 .map(|l| l.into_iter().map(|b| if b { 1 } else { 0 }).sum::<u32>())
                 .sum();
         }
-        let mut new_beams: Vec<((isize, isize), Direction)> = vec![];
-        for beam_idx in 0..beams.len() {
-            let ((x, y), direction) = beams[beam_idx];
-            // skip beams that have gone outside or are already visited
-            if x < 0
-                || grid.len() <= (x as usize)
-                || y < 0
-                || grid[0].len() <= (y as usize)
-                || visited.contains(&((x, y), direction))
-            {
-                continue;
-            }
-            visited.insert(((x, y), direction));
-            energized_grid[x as usize][y as usize] = true;
-            let tile = grid[x as usize][y as usize];
+        let mut new_beams: Vec<(isize, isize, Direction)> = vec![];
+        beams = beams
+            .into_iter()
+            .filter_map(|beam| {
+                let (x, y, direction) = beam;
+                // skip beams that have gone outside or are already visited
+                if x < 0
+                    || grid.len() <= (x as usize)
+                    || y < 0
+                    || grid[0].len() <= (y as usize)
+                    || visited.contains(&(x, y, direction))
+                {
+                    return None;
+                }
+                visited.insert((x, y, direction));
+                energized_grid[x as usize][y as usize] = true;
+                let tile = grid[x as usize][y as usize];
 
-            match (tile, direction) {
-                (Tile::Empty | Tile::SplitH, Direction::East) => {
-                    beams[beam_idx] = ((x, y + 1), direction)
+                match (tile, direction) {
+                    (Tile::Empty | Tile::SplitH, Direction::East) => Some((x, y + 1, direction)),
+                    (Tile::Empty | Tile::SplitV, Direction::South) => Some((x + 1, y, direction)),
+                    (Tile::Empty | Tile::SplitH, Direction::West) => Some((x, y - 1, direction)),
+                    (Tile::Empty | Tile::SplitV, Direction::North) => Some((x - 1, y, direction)),
+                    //       \
+                    (Tile::MirrorNWSE, Direction::East) => Some((x + 1, y, Direction::South)),
+                    (Tile::MirrorNWSE, Direction::South) => Some((x, y + 1, Direction::East)),
+                    (Tile::MirrorNWSE, Direction::West) => Some((x - 1, y, Direction::North)),
+                    (Tile::MirrorNWSE, Direction::North) => Some((x, y - 1, Direction::West)),
+                    //       /
+                    (Tile::MirrorSWNE, Direction::East) => Some((x - 1, y, Direction::North)),
+                    (Tile::MirrorSWNE, Direction::South) => Some((x, y - 1, Direction::West)),
+                    (Tile::MirrorSWNE, Direction::West) => Some((x + 1, y, Direction::South)),
+                    (Tile::MirrorSWNE, Direction::North) => Some((x, y + 1, Direction::East)),
+                    (Tile::SplitH, _) => {
+                        new_beams.push((x, y + 1, Direction::East));
+                        Some((x, y - 1, Direction::West))
+                    }
+                    (Tile::SplitV, _) => {
+                        new_beams.push((x + 1, y, Direction::South));
+                        Some((x - 1, y, Direction::North))
+                    }
                 }
-                (Tile::Empty | Tile::SplitV, Direction::South) => {
-                    beams[beam_idx] = ((x + 1, y), direction)
-                }
-                (Tile::Empty | Tile::SplitH, Direction::West) => {
-                    beams[beam_idx] = ((x, y - 1), direction)
-                }
-                (Tile::Empty | Tile::SplitV, Direction::North) => {
-                    beams[beam_idx] = ((x - 1, y), direction)
-                }
-                //       \
-                (Tile::MirrorNWSE, Direction::East) => {
-                    beams[beam_idx] = ((x + 1, y), Direction::South)
-                }
-                (Tile::MirrorNWSE, Direction::South) => {
-                    beams[beam_idx] = ((x, y + 1), Direction::East)
-                }
-                (Tile::MirrorNWSE, Direction::West) => {
-                    beams[beam_idx] = ((x - 1, y), Direction::North)
-                }
-                (Tile::MirrorNWSE, Direction::North) => {
-                    beams[beam_idx] = ((x, y - 1), Direction::West)
-                }
-                //       /
-                (Tile::MirrorSWNE, Direction::East) => {
-                    beams[beam_idx] = ((x - 1, y), Direction::North)
-                }
-                (Tile::MirrorSWNE, Direction::South) => {
-                    beams[beam_idx] = ((x, y - 1), Direction::West)
-                }
-                (Tile::MirrorSWNE, Direction::West) => {
-                    beams[beam_idx] = ((x + 1, y), Direction::South)
-                }
-                (Tile::MirrorSWNE, Direction::North) => {
-                    beams[beam_idx] = ((x, y + 1), Direction::East)
-                }
-                (Tile::SplitH, _) => {
-                    beams[beam_idx] = ((x, y - 1), Direction::West);
-                    new_beams.push(((x, y + 1), Direction::East));
-                }
-                (Tile::SplitV, _) => {
-                    beams[beam_idx] = ((x - 1, y), Direction::North);
-                    new_beams.push(((x + 1, y), Direction::South));
-                }
-            }
-        }
-        beams.extend(
-            new_beams
-                .clone()
-                .into_iter()
-                .filter(|b| !visited.contains(b)),
-        );
+            })
+            .collect_vec();
+        beams.extend(new_beams);
     }
 }
 
@@ -129,39 +97,30 @@ fn parse_input(input: &str) -> (u32, u32) {
         })
         .collect_vec();
 
-    let part1 = get_energized_tiles(&grid, ((0, 0), Direction::East));
+    let part1 = get_energized_tiles(&grid, (0, 0, Direction::East));
 
-    let part2 = Direction::iter()
-        .map(|direction| match direction {
-            Direction::East => (0..grid.len())
-                .map(|x| get_energized_tiles(&grid, ((x as isize, 0), Direction::East)))
-                .max()
-                .unwrap(),
-            Direction::South => (0..grid[0].len())
-                .map(|y| get_energized_tiles(&grid, ((0, y as isize), Direction::South)))
-                .max()
-                .unwrap(),
-            Direction::West => (0..grid.len())
-                .map(|x| {
-                    get_energized_tiles(
-                        &grid,
-                        ((x as isize, grid[0].len() as isize), Direction::West),
-                    )
-                })
-                .max()
-                .unwrap(),
-            Direction::North => (0..grid[0].len())
-                .map(|y| {
-                    get_energized_tiles(
-                        &grid,
-                        ((grid.len() as isize, y as isize), Direction::North),
-                    )
-                })
-                .max()
-                .unwrap(),
+    let rows = (0..grid.len())
+        .into_par_iter()
+        .map(|x| {
+            get_energized_tiles(&grid, (x as isize, 0, Direction::East)).max(get_energized_tiles(
+                &grid,
+                (x as isize, grid[0].len() as isize, Direction::West),
+            ))
         })
         .max()
         .unwrap();
+    let columns = (0..grid[0].len())
+        .into_par_iter()
+        .map(|y| {
+            get_energized_tiles(&grid, (0, y as isize, Direction::South)).max(get_energized_tiles(
+                &grid,
+                (grid.len() as isize, y as isize, Direction::North),
+            ))
+        })
+        .max()
+        .unwrap();
+
+    let part2 = rows.max(columns);
 
     (part1, part2)
 }
