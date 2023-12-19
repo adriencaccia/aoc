f8edcfcec22c109398775f94b2bd5eda975ee208
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-19 08:59:02 +0100

    refactor(2023): expose crates in lib instead of main

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,23 +1,5 @@
 use clap::Parser;
-
-mod day01;
-mod day02;
-mod day03;
-mod day04;
-mod day05;
-mod day06;
-mod day07;
-mod day08;
-mod day09;
-mod day10;
-mod day11;
-mod day12;
-mod day13;
-mod day14;
-mod day15;
-mod day16;
-mod day17;
-mod day18;
+use rust_2023::*;
 
 const N_DAYS: u8 = 25;
 
