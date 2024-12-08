69e4b686cc4ce38ff0904669a550c8f71fcec822
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2024-12-08 22:47:52 +0100

    feat(2024): day 8 remove grid and easier parsing

--- 2024/src/day8/mod.rs
+++ 2024/src/day8/mod.rs
@@ -3,35 +3,53 @@ use std::collections::HashMap;
 use itertools::Itertools;
 
 const GRID_SIZE: usize = 50;
-const CHARS_LENGTH: usize = 53;
+const CHARS_LENGTH: usize = 26 + 26 + 10;
 const CHAR_OCC: usize = 4; // each antenna has at most 4 occurrences
 
 type CharPositions = HashMap<u8, Vec<(usize, usize)>>;
-type Grid = [[u8; GRID_SIZE]; GRID_SIZE];
 
-fn parse(input: &str) -> (CharPositions, Grid) {
-    let mut grid = [[b' '; GRID_SIZE]; GRID_SIZE];
+fn parse(input: &str) -> (CharPositions, usize) {
     let mut chars = CharPositions::with_capacity(CHARS_LENGTH);
+    let mut size = 0;
     input.lines().enumerate().for_each(|(i, line)| {
         line.bytes().enumerate().for_each(|(j, c)| {
-            grid[i][j] = c;
             if c == b'.' {
                 return;
             }
-            if let Some(positions) = chars.get_mut(&c) {
-                positions.push_within_capacity((i, j)).unwrap();
-            } else {
-                let mut positions = Vec::with_capacity(CHAR_OCC);
-                positions.push_within_capacity((i, j)).unwrap();
-                chars.insert(c, positions);
-            }
+            chars
+                .entry(c)
+                .or_insert_with(|| Vec::with_capacity(CHAR_OCC))
+                .push_within_capacity((i, j))
+                .unwrap();
         });
+        size += 1;
     });
-    (chars, grid)
+    (chars, size)
+}
+
+#[inline(always)]
+fn try_add_antenna(
+    size: usize,
+    antenna: (isize, isize),
+    new_antennas: &mut u32,
+    a_nodes: &mut [[bool; 50]; 50],
+) {
+    if antenna.0 >= 0
+        && (antenna.0 as usize) < size
+        && antenna.1 >= 0
+        && (antenna.1 as usize) < size
+    {
+        let i = antenna.0 as usize;
+        let j = antenna.1 as usize;
+        if !a_nodes[i][j] {
+            *new_antennas += 1;
+            a_nodes[i][j] = true;
+        }
+    }
 }
 
 pub fn part1(input: &str) -> u32 {
-    let (chars, mut grid) = parse(input);
+    let (chars, size) = parse(input);
     let mut a_nodes = [[false; GRID_SIZE]; GRID_SIZE];
 
     chars.into_iter().fold(0, |sum, (_, p)| {
@@ -42,43 +60,18 @@ pub fn part1(input: &str) -> u32 {
             let a_1 = ((*a as isize) - 2 * i_diff, (*b as isize) - 2 * j_diff);
             let a_2 = ((*c as isize) + 2 * i_diff, (*d as isize) + 2 * j_diff);
 
-            try_add_antenna(a_1, &mut grid, &mut new_antennas, &mut a_nodes);
-            try_add_antenna(a_2, &mut grid, &mut new_antennas, &mut a_nodes);
+            try_add_antenna(size, a_1, &mut new_antennas, &mut a_nodes);
+            try_add_antenna(size, a_2, &mut new_antennas, &mut a_nodes);
         });
 
         sum + new_antennas
     })
 }
 
-fn try_add_antenna(
-    antenna: (isize, isize),
-    grid: &mut [[u8; 50]; 50],
-    new_antennas: &mut u32,
-    a_nodes: &mut [[bool; 50]; 50],
-) {
-    if antenna.0 >= 0
-        && (antenna.0 as usize) < GRID_SIZE
-        && antenna.1 >= 0
-        && (antenna.1 as usize) < GRID_SIZE
-    {
-        let i = antenna.0 as usize;
-        let j = antenna.1 as usize;
-        match grid[i][j] {
-            // empty check is just for example
-            b' ' => {}
-            _ if !a_nodes[i][j] => {
-                *new_antennas += 1;
-                a_nodes[i][j] = true;
-            }
-            _ => {}
-        }
-    }
-}
-
 // ByteMap ???
 // bitset grid to store a_nodes aswell, no duplicate
 pub fn part2(input: &str) -> u32 {
-    let (chars, mut grid) = parse(input);
+    let (chars, size) = parse(input);
     let mut a_nodes = [[false; GRID_SIZE]; GRID_SIZE];
 
     chars.into_iter().fold(0, |sum, (_, p)| {
@@ -86,11 +79,11 @@ pub fn part2(input: &str) -> u32 {
         p.iter().tuple_combinations().for_each(|((a, b), (c, d))| {
             let i_diff = (*a as isize) - (*c as isize);
             let j_diff = (*b as isize) - (*d as isize);
-            for x in 0..30 {
+            for x in 0..40 {
                 let a_1 = ((*a as isize) - x * i_diff, (*b as isize) - x * j_diff);
                 let a_2 = ((*c as isize) + x * i_diff, (*d as isize) + x * j_diff);
-                try_add_antenna(a_1, &mut grid, &mut new_antennas, &mut a_nodes);
-                try_add_antenna(a_2, &mut grid, &mut new_antennas, &mut a_nodes);
+                try_add_antenna(size, a_1, &mut new_antennas, &mut a_nodes);
+                try_add_antenna(size, a_2, &mut new_antennas, &mut a_nodes);
             }
         });
 
